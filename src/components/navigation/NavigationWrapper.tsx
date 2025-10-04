d02bd78c2ba5df6669670d9b197b354b31c84305
judahwon <hwondaa@gmail.com>
2025-10-04 15:13:14 +0900

    [Style] : logo 추가한다

--- src/components/navigation/NavigationWrapper.tsx
+++ src/components/navigation/NavigationWrapper.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Logo from '@/components/icons/Logo';
 import DebugSidebar from '@/components/navigation/DebugSidebar';
 import UploadSidebar from '@/components/navigation/UploadSidebar';
 import { ExtendedLocationData } from '@/types';
@@ -21,8 +22,7 @@ export default function NavigationWrapper({
 }: NavigationWrapperProps) {
   return (
     <div className="bg-background-90 absolute left-6 top-6 flex flex-col gap-4 z-10 w-72 shadow-lg rounded-lg p-4 border border-gray-7">
-      <h1 className="text-xl font-bold text-accent-1">{'Zizizip'}</h1>
-
+      <Logo />
       {/* 사이드바 */}
       <UploadSidebar onDataUploaded={onDataUploaded} />
 
