9dbe064d00092cadfa5bd63ed67ce517c8b8caeb
judahwon <hwondaa@gmail.com>
2025-10-06 20:53:23 +0900

    [Fix] : metadata 타이틀 수정한다

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -4,7 +4,7 @@ import MapPageClient from '@/components/map/MapPageClient';
 
 export function generateMetadata(): Metadata {
   return {
-    title: mapPageMetadata.title,
+    title: '지지집 - 간편한 부동산 위치 지도',
     description: mapPageMetadata.description,
     alternates: {
       canonical: mapPageMetadata.url,
