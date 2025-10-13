463f34cda0e50d7c61f3b51b0ecb40bca8324483
judahwon <hwondaa@gmail.com>
2025-10-13 14:41:28 +0900

    [Feat] : /map의 header 클릭 시, 첫 화면으로 리다이렉트한다

--- src/components/navigation/NavigationWrapper.tsx
+++ src/components/navigation/NavigationWrapper.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import Logo from '@/components/icons/Logo';
+import { useRouter } from 'next/navigation';
 // import DebugSidebar from '@/components/navigation/DebugSidebar';
-import UploadSidebar from '@/components/navigation/UploadSidebar';
 import { ExtendedLocationData } from '@/types';
+import Logo from '@/components/icons/Logo';
+import UploadSidebar from '@/components/navigation/UploadSidebar';
 
 interface NavigationWrapperProps {
   showAllMarkers: boolean;
@@ -20,17 +21,23 @@ export default function NavigationWrapper({
   // onToggleDebugInfo,
   onDataUploaded,
 }: NavigationWrapperProps) {
+  const router = useRouter();
   return (
     <>
 
       <div className="absolute left-6 top-5 flex flex-col gap-4 z-10 w-72">
-        <div className="flex items-center gap-2 z-10">
-          <Logo />
-          <span className="flex flex-col font-bold text-stroke text-sub">
-            {'지지집'}
-            <span className="text-sm text-gray-1 mt-[-4px]">{'엑셀로 된 부동산 정보를 지도에 표출하세요'}</span>
-          </span>
-        </div>
+        <header>
+          <button
+            onClick={() => router.push('/')}
+            className="flex items-center gap-2 z-10"
+          >
+            <Logo />
+            <h1 className="flex flex-col items-start font-bold text-stroke text-sub">
+              {'지지집'}
+              <span className="text-sm text-gray-1 mt-[-4px]">{'엑셀로 된 부동산 정보를 지도에 표출하세요'}</span>
+            </h1>
+          </button>
+        </header>
         {/* 사이드바 */}
         <UploadSidebar onDataUploaded={onDataUploaded} />
 
