1780994f30a56d3a0d72714a0f61b2d4d71e5433
judahwon <hwondaa@gmail.com>
2025-11-17 17:24:56 +0900

    [Feat] : 네이버 검색 등록한다

---
+++ src/constants/NaverSiteVerification.tsx
@@ -0,0 +1,7 @@
+const NaverSiteVerification = () => {
+  return (
+    <meta name="naver-site-verification" content="47d4bf02ed5de6a65bf189007ee6a615d1e435d8" />
+  );
+};
+
+export default NaverSiteVerification;
